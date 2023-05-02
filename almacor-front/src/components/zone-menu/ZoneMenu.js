e39d3d21ff0dd6e9277b81c2b5fc6f349f8e7b9e
Matias-C <55718713+Matias-C@users.noreply.github.com>
2023-05-01 21:25:13 -0300

    DEVELOP_update

--- almacor-front/src/components/zone-menu/ZoneMenu.js
+++ almacor-front/src/components/zone-menu/ZoneMenu.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, useRef } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate, Outlet } from 'react-router-dom';
 
 import Button from '@mui/material/Button';
--- almacor-front/src/pages/search/SearchPalletPage.js
+++ almacor-front/src/pages/search/SearchPalletPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
